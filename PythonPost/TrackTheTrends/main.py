2c985b7e91d98d0b9ece768062b2ae8f98dc81ff
HeartLightBoXu <HeartLightBoXu@users.noreply.github.com>
2019-03-08 15:50:12 +0800

    实现拟合分析
    拟合分析初步实现，效果不详

--- PythonPost/TrackTheTrends/main.py
+++ PythonPost/TrackTheTrends/main.py
@@ -17,35 +17,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+slope_five_list=[]
+
 # datas=sd.create_data(100)
 data_frame=pd.read_csv('top_speed28.csv')
 datas=list(data_frame['number'])
 # ssd=SimpleSingleDouble()
 ssd=SumSingleDouble()
-ssd.appends(datas[:200])
-x=np.arange(0,200,1)
+ssd.appends(datas[:100])
+x=np.arange(0,100,1)
 # y=ssd.sum_list_double
 y=ssd.sum_list
 
-plt.scatter(x,y)
-fn=pm.keep_r_squared(x,y)
-plt.plot(x,fn(x))
-plt.show()
+# plt.plot(x,y)
+# fn=pm.keep_r_squared(x,y)
+fn=pm.between_r_squared(x,y)
+# plt.plot(x,fn(x))
+# plt.show()
 
 index=1
 
+_fn=fn.deriv(m = 1)
+slope_five=_fn(x[100-5:])
+slope_five_list.append(slope_five)
+
 while True:
-    plt.cla()
+    # plt.cla()
 
     # data=sd.create_data()
-    data=datas[200+index-1:200+index]
+    data=datas[100+index-1:100+index]
     ssd.appends(data)
-    x=np.arange(0,200+index,1)
+    x=np.arange(0,100+index,1)
     # y=ssd.sum_list_double
     y=ssd.sum_list
 
-    plt.scatter(x,y)
-    fn=pm.keep_r_squared(x,y)
-    plt.plot(x,fn(x))
-    plt.show()
+    # plt.plot(x,y)
+    # fn=pm.keep_r_squared(x,y)
+    fn=pm.between_r_squared(x,y)
+    # plt.plot(x,fn(x))
+    # plt.show()
     index+=1
+    _fn=fn.deriv(m = 1)
+    slope_five=_fn(x[100+index-6:])
+    slope_five_list.append(slope_five)
+
+    if index>150:
+        break
+
+slope_five_array=np.array(slope_five_list)
+
+dataframe = pd.DataFrame({'data':ssd.data,
+    'post_list':ssd.post_list,
+    'sum_list':ssd.sum_list})
+dataframe.to_csv('PythonPost/TrackTheTrends/sum_single_double_data.csv',index=False,sep=',')
+
+dataframe = pd.DataFrame(slope_five_array, columns=['01', '02', '03', '04', '05']) 
+dataframe.to_csv('PythonPost/TrackTheTrends/sum_single_double_result.csv',index=False,sep=',')
+
+print('end')
--- PythonPost/TrackTheTrends/polyomial_manager.py
+++ PythonPost/TrackTheTrends/polyomial_manager.py
@@ -50,13 +50,16 @@ def between_r_squared(x,y):
     global curr_r_squared
     global first
 
+    index=0
+    
     while True:
+        index+=1
         if first:
             results=exnp.ex_polyfit(x,y,step)
             curr_r_squared=results['determination']
             fn=np.poly1d(results['polynomial'])
-            if curr_r_squared>=0.85:
-                print('First step:%d squared:%f'%(step,curr_r_squared))
+            if curr_r_squared>=0.90:
+                # print('First step:%d squared:%f'%(step,curr_r_squared))
                 first=False
                 return fn
             step+=1
@@ -64,15 +67,17 @@ def between_r_squared(x,y):
             results=exnp.ex_polyfit(x,y,step)
             r_squared=results['determination']
             fn=np.poly1d(results['polynomial'])
-            if r_squared>=curr_r_squared:
+            if r_squared>=0.90 and r_squared<=0.92:
                 curr_r_squared=r_squared
-                print('First step:%d squared:%f'%(step,curr_r_squared))
+                # print('First step:%d squared:%f'%(step,curr_r_squared))
                 return fn
-            else:
+            elif r_squared<0.95:
                 step+=1
-                results=exnp.ex_polyfit(x,y,step)
-                r_squared=results['determination']
-                fn=np.poly1d(results['polynomial'])
-                curr_r_squared=r_squared
-                print('First step:%d squared:%f'%(step,curr_r_squared))
+                if index>100:
+                    return fn
+            elif step>2:
+                step-=1
+                if index>10:
+                    return fn
+            else:
                 return fn
